d0327d0d3b64a0270ba0ace16fa9ee234959ea67
Oleh <musiychuk09@gmail.com>
2024-09-12 10:33:13 +0300

    register all models in admin.py

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from task_manager.models import Task, Project, Position, Worker, Team, TaskType
+
+admin.site.register(Task)
+admin.site.register(Project)
+admin.site.register(Position)
+admin.site.register(Worker)
+admin.site.register(Team)
+admin.site.register(TaskType)
