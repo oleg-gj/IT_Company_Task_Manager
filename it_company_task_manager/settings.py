a5e8a22348326b4c1021814eb3c3d701fc11b353
Oleh <musiychuk09@gmail.com>
2024-09-12 10:32:27 +0300

    add "crispy_bootstrap4" to INSTALLED_APPS and CRISPY_TEMPLATE_PACK in settings.py

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_bootstrap4",
     "crispy_forms",
     "task_manager",
 
@@ -131,3 +132,5 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "task_manager.Worker"
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
