bfde767f76bfa1528f389be5bcb5ba6a34ddac0f
Oleh <musiychuk09@gmail.com>
2024-09-13 15:35:34 +0300

    add create,update,delete,detail view for Worker,Team,Project, Task, TaskType, Position models and add get_context_data, get_queryset for TaskListView and WorkerListView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
 from django.views import generic
+
+from task_manager.forms import (
+    TaskNameSearchForm,
+    WorkerNameSearchForm,
+    WorkerCreationForm,
+    WorkerUpdateForm,
+    TaskUpdateForm
+)
 
 from task_manager.models import Team, Project, Worker, Task, TaskType, Position
 
@@ -33,24 +42,193 @@ class TeamListView(generic.ListView):
     paginate_by = 5
 
 
+class TeamDetailView(generic.DetailView):
+    model = Team
+
+
+class TeamCreateView(generic.CreateView):
+    model = Team
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:team-list")
+
+
+class TeamUpdateView(generic.UpdateView):
+    model = Team
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:team-list")
+
+
+class TeamDeleteView(generic.DeleteView):
+    model = Team
+    success_url = reverse_lazy("task_manager:team-list")
+
+
 class ProjectListView(generic.ListView):
     model = Project
     paginate_by = 5
+
+
+class ProjectDetailView(generic.DetailView):
+    model = Project
+
+
+class ProjectCreateView(generic.CreateView):
+    model = Project
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:project-list")
+
+
+class ProjectUpdateView(generic.UpdateView):
+    model = Project
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:project-list")
+
+
+class ProjectDeleteView(generic.DeleteView):
+    model = Project
+    success_url = reverse_lazy("task_manager:project-list")
 
 
 class WorkerListView(generic.ListView):
     model = Worker
     paginate_by = 5
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(WorkerListView, self).get_context_data(**kwargs)
+        first_name = self.request.GET.get("first_name", "")
+        context["first_name"] = first_name
+        context["search_form"] = WorkerNameSearchForm(
+            initial={"first_name": first_name}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Worker.objects.all()
+        form = WorkerNameSearchForm(self.request.GET)
+
+        if form.is_valid():
+            queryset = queryset.filter(
+                first_name__icontains=form.cleaned_data["first_name"]
+            )
+        return queryset
+
+
+class WorkerDetailView(generic.DetailView):
+    model = Worker
+
+
+class WorkerCreateView(generic.CreateView):
+    model = Worker
+    form_class = WorkerCreationForm
+    success_url = reverse_lazy("task_manager:worker-list")
+
+
+class WorkerUpdateView(generic.UpdateView):
+    model = Worker
+    form_class = WorkerUpdateForm
+
+    def get_success_url(self, **kwargs):
+        return reverse("task_manager:worker-detail",
+                       kwargs={'pk': self.kwargs['pk']})
+
+
+class WorkerDeleteView(generic.DeleteView):
+    model = Worker
+    success_url = reverse_lazy("task_manager:worker-list")
+
 
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["name"] = name
+        context["search_form"] = TaskNameSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        form = TaskNameSearchForm(self.request.GET)
+
+        if form.is_valid():
+            queryset = queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return queryset
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskUpdateForm
+    success_url = reverse_lazy("task_manager:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task_manager:task-list")
+
+
 class TaskTypeListView(generic.ListView):
     model = TaskType
     paginate_by = 5
 
+
+class TaskTypeDetailView(generic.DetailView):
+    model = TaskType
+
+
+class TaskTypeCreateView(generic.CreateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:tasktype-list")
+
+
+class TaskTypeUpdateView(generic.UpdateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:tasktype-list")
+
+
+class TaskTypeDeleteView(generic.DeleteView):
+    model = TaskType
+    success_url = reverse_lazy("task_manager:tasktype-list")
+
+
 class PositionListView(generic.ListView):
     model = Position
     paginate_by = 5
+
+
+class PositionDetailView(generic.DetailView):
+    model = Position
+
+
+class PositionCreateView(generic.CreateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:position-list")
+
+
+class PositionUpdateView(generic.UpdateView):
+    model = Position
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:position-list")
+
+
+class PositionDeleteView(generic.DeleteView):
+    model = Position
+    success_url = reverse_lazy("task_manager:position-list")
